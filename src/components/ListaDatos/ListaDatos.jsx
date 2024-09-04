d497b7e50a1db7c5099ed70e7e750a315975bc88
Lautaro Montaña <joselautarom@gmail.com>
2024-09-04 03:06:40 -0300

    Update en lista de datos

--- src/components/ListaDatos/ListaDatos.jsx
+++ src/components/ListaDatos/ListaDatos.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { FaRegTrashAlt } from "react-icons/fa";
 import { FaPencilAlt } from "react-icons/fa";
-import './ListaDatos.css'
+import './ListaDatos.css';
 
 export const ListaDatos = ({ datos, eliminarDato, editarDato }) => {
   return (
     <>
-    <h2>Datos enviados:</h2>
-      <div>
-        {datos.map((dato, index) =>(
-          <div className='div-container' key={index}>
-            <p>Nombre: {dato.nombre}</p>
-            <p>Apellido: {dato.apellido}</p>
-            <p>Email: {dato.email}</p>
-            <FaPencilAlt className='pencil' onClick={() => editarDato(index)}/>
-            <FaRegTrashAlt className='trash' onClick={() => eliminarDato(index)} />
-          </div>
+      <h2>Datos enviados:</h2>
+      <table className='datos-table'>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Email</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {datos.map((dato, index) => (
+            <tr key={index}>
+              <td>{dato.nombre}</td>
+              <td>{dato.apellido}</td>
+              <td>{dato.email}</td>
+              <td>
+                <FaPencilAlt className='pencil' onClick={() => editarDato(index)} />
+                <FaRegTrashAlt className='trash' onClick={() => eliminarDato(index)} />
+              </td>
+            </tr>
           ))}
-      </div>
+        </tbody>
+      </table>
     </>
-  )
+  );
 }
+
